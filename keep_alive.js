8687c4b2c25029c1b55ed5b7080d7f44b5dfdde8
Allani Mohamed <68595124+ALL4N1@users.noreply.github.com>
2024-06-11 10:00:24 +0100

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -3,15 +3,19 @@ const fs = require('fs');
 const { Client, GatewayIntentBits } = require('discord.js');
 
 // Server setup
+const port = process.env.PORT || 8080;
 const server = http.createServer((req, res) => {
   if (req.method === 'GET') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end("I'm alive");
+  } else {
+    res.writeHead(405, { 'Content-Type': 'text/html' });
+    res.end("Method Not Allowed");
   }
 });
 
-server.listen(8080, '0.0.0.0', () => {
-  console.log('Server running!');
+server.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on port ${port}`);
 });
 
 // Discord bot setup
@@ -25,7 +29,7 @@ const client = new Client({
   ],
 });
 
-client.once('ready', () => {
+client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 client.login("MTI0OTk3ODMzMjA0NTYzOTcxMQ.GR_oTb.tRMENjAv5k8iFMIEhWLJWtHVJQzywJQ4yYDB2Q")
