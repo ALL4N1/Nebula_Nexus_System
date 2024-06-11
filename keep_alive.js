09d4f687bfdfe1b6fc4dd4dbed0d07b3df69ec6d
Allani Mohamed <68595124+ALL4N1@users.noreply.github.com>
2024-06-11 09:38:00 +0100

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,14 +1,44 @@
-const fs = require('fs');
-const Discord = require('discord.js')
-
-var http = require('http');
-http.createServer(function (req, res) {
-  res.write("I'm alive");
-  res.end();
-}).listen(8080);
-
-const client = new Discord.Client();
-client.on('ready', () => {
-	console.log('logged on')
+const http = require('http');
+const server = http.createServer(receive_req);
+server.listen(8080, '0.0.0.0', () => {
+  console.log(`Server running!`);
 });
+
+function receive_req(req, res) {
+  if (req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end('Post basic authenticaton redirector by RTS. ' + Math.random());
+  }
+}
+
+
+const fs = require('fs');
+const Discord = require("discord.js");
+const {
+  Client,
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  ModalBuilder,
+  TextInputStyle,
+  TextInputBuilder,
+} = require("discord.js");
+const { MessageButton } = require("discord.js");
+const client = new Client({
+  intents: [
+    "Guilds",
+    "MessageContent",
+    "GuildMessages",
+    "GuildMembers",
+    "GuildVoiceStates",
+  ],
+});
+
+//---------------------------------------------//
+
+console.log("Project is running");
+client.on("ready", () =>
+  console.log(`logged in as - ${client.user.username} - `),
+);
 client.login("MTI0OTk3ODMzMjA0NTYzOTcxMQ.GR_oTb.tRMENjAv5k8iFMIEhWLJWtHVJQzywJQ4yYDB2Q")
